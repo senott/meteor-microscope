6c39e564bfafc3e67c7112a86be63abc74cb867b
Senott <senott@yahoo.com>
2016-02-07 21:08:00 -0200

    @DASMacBookAir: adição do formulário de edição de artigos.

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,4 +1,8 @@
 Template.postItem.helpers({
+  ownPost: function() {
+    //TODO: alterar userID para userId
+    return this.userID == Meteor.userId();
+  },
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
