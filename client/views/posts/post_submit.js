43800f2e82e646bf8748448d402339d93dfb2ecb
Senott <senott@yahoo.com>
2016-02-21 17:00:05 -0300

    @DASMacBookAir: 9.5.1 created basic errors package and linked it it.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -24,12 +24,12 @@ Template.postSubmit.events({
     Meteor.call('post', post, function(error, id) {
       if (error) {
         // display the error to the user
-        throwError(error.reason);
+        Error.throw(error.reason);
 
         if (error.error === 302)
           Router.go('postPage', {_id: error.details})
       } else {
-        Router.go('postPage', {_id: id});  
+        Router.go('postPage', {_id: id});
       }
     });
   }
